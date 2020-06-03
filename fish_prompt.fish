847387b6f37c812b036baa3cadec18dd2865b7ab
Nikhil CSB <csbnikhil@gmail.com>
2020-06-04 00:46:31 +0530

    three red arrows for rc != 0

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,14 +1,14 @@
 function fish_prompt
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    if test $status -ne 0
+        echo -n (set_color red)'❯❯❯ '
+    else
+        echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    end
     set_color normal
 end
 
 function fish_right_prompt
-    if test ! $status -eq 0
-        set_color red
-    else
-        set_color blue
-    end
+    set_color blue
     basename (prompt_pwd)
     set_color normal
 end
