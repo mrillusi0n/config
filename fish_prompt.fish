de39150d9e9c2a9412b5ffdcf1880f75b9eb0c25
Nikhil CSB <csbnikhil@gmail.com>
2020-06-14 15:41:06 +0530

    remove redundance

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -8,11 +8,6 @@ function fish_prompt
 end
 
 function fish_right_prompt
-    set -l cmd_status $status
-    if test $cmd_status -ne 0
-        echo -n (set_color red)"✘ $cmd_status"
-    end
-
     if not command -sq git
         set_color normal
         return
