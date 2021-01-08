ca7518794c4a6e40a9544efe5dff0f62c56b19a0
Nikhil CSB <csbnikhil@gmail.com>
2021-01-08 22:22:36 +0530

    track the whole fish config

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+set -x DISCORD_TOKEN Nzc3MDU5NzU1MTIxOTY3MTE0.X697Sg.CucCD1YTg6tykxlVrGSxXfxk01g
+
+# opam configuration
+source /Users/Nick/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
