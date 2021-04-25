66f088f9e508d1bf60f66e2f1e9c6c50035f4710
Nikhil CSB <csbnikhil@gmail.com>
2021-04-26 00:51:28 +0530

    color changes

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt
     if test $status -ne 0
         set_color red
     else
-        set_color yellow
+        set_color brgreen
     end
     echo -n "><> "
     set_color normal
