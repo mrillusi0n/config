dc554d349cc797b6e55dab63f6809ea9527f781b
mrillusi0n <csbnikhil@gmail.com>
2021-07-31 02:29:28 +0530

    correct naming

--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,5 +1,5 @@
-function venv --description "<name of the virtual env>"
-	set venv_home "$HOME/venv"
-	echo "Choosing Python Virtual Env: $venv_home/$argv[1]/bin/activate.fish"
-	source "$venv_home/$argv[1]/bin/activate.fish"
+function venv --description "<path to the virtual env>"
+	set VIRTUAL_ENVS "$HOME/venv"
+	echo "Choosing Python Virtual Env: $VIRTUAL_ENVS/$argv[1]/bin/activate.fish"
+	source "$VIRTUAL_ENVS/$argv[1]/bin/activate.fish"
 end
